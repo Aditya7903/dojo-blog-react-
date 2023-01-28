a7b753fcec172555036b1499254b4199b9be23b8
unknown <aviartstudio121@gmail.com>
2023-01-28 19:44:27 +0530

    Second change

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@
 import './App.css';
 
 function App() {
+  const title ='Welcome to my new blog';
+  const likes =50;
+  // const person = {name:'yosh', age:30};
+  const link ="http://www.google.com";
   return (
     <div className="App">
      <div className='content'>
-      <h1>App Component</h1>
+      <h1>{title}</h1>
+      <p>liked {likes} times</p>
+      {/* <p>{person}</p> */}
+      <a href={link}>google site</a>
      </div>
     </div>
   );
